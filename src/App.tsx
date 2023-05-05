0f1f58b57eb99ad5a63b716d9fffe9dc87f833cf
kozMikhailS1286 <koz.mikhail.s@yandex.ru>
2023-05-05 17:40:47 +0300

    5.05.23 Finish.

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,19 @@ import './App.css';
 import Header from "./header/Header";
 import Main from "./main/Main";
 import Skills from "./skills/Skills";
-import Works from "./works/Works";
+import Projects from "./projects/Projects";
 import Find from "./find/Find";
 import Contacts from "./contakts/Contacts";
 import Footer from "./footer/Footer";
 
+
 function App() {
     return (
         <div className="App">
-            <Header/>
-            <Main/>
+            <Header />
+            <Main />
             <Skills />
-            <Works />
+            <Projects />
             <Find />
             <Contacts />
             <Footer />
