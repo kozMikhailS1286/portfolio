409b494a1fdfd02dc9fc22a964ee6b3ee39080fc
kozMikhailS1286 <koz.mikhail.s@yandex.ru>
2023-07-31 15:59:42 +0300

    31.07.23 / 2

---
+++ src/projects/Projects.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import style from './Projects.module.css';
+import styleContainer from "../common/styles/Container.module.css";
+import Project from "./project/Project";
+import Title from "../common/components/title/Title";
+import todoImage from './../assets/image/Todo.jpg';
+import counterImage from './../assets/image/Counter.jpg';
+
+
+function Projects() {
+    const todo = {
+        backgroundImage: `url(${todoImage})`,
+    };
+
+    const counter = {
+        backgroundImage: `url(${counterImage})`,
+    };
+
+    return (
+        <div className={style.worksBlock}>
+            <div className={`${styleContainer.container} ${style.skillsContainer}`}>
+                <Title text={"Project"}/>
+                <div className={style.works}>
+                    <Project
+                                style={todo}
+                                title={"Project name1"}
+                                description={"Description1"}
+                    />
+                    <Project
+                                style={counter}
+                                title={"Project name2"}
+                                description={"Description2"}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Projects;
---
+++ src/projects/project/Project.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import style from './Project.module.css';
+
+
+type PropsType = {
+    title: string
+    description: string
+    style?: any
+}
+function Project(props: PropsType) {
+    return (
+        <div>
+            <div className={style.container}>
+                <div className={style.image} style={props.style}>
+                    <a href={''}> <button className={style.btn}> Смотреть </button> </a>
+                </div>
+            </div>
+            <div className={style.projectInfo}>
+                <h3 className={style.projectTitle}> {props.title} </h3>
+                <span className={style.description}>
+                {props.description}
+            </span></div>
+        </div>
+    )
+}
+
+export default Project;
